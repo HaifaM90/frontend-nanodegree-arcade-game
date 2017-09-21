b22cca2114b396b34b46840202750b423de0503a
Haifa <hma@Haifas-MacBook-Pro.local>
2017-09-22 01:50:54 +0300

    clean up

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var Enemy = function(x,y) {
   this.sprite = 'images/enemy-bug.png';
   this.y=y;
   this.x=x;
-  this.speed= Math.floor(Math.random() * (30 - 6) + 6);
+  this.speed= Math.floor(Math.random() * (30 - 6) + 6); //chose random speed to each enemy
   this.width=171;
   this.height=101;
 };
@@ -19,6 +19,7 @@ Enemy.prototype.update = function(dt) {
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
+  //keep enemies moves
   if (this.x<450)
   this.x+= this.speed * dt;
   else
@@ -30,7 +31,7 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
+//collisions method use common algrothim and update it as need it
 Enemy.prototype.checkCollisions = function()
 {
   if (this.x < player.x + player.width/2.25 &&
@@ -39,8 +40,8 @@ Enemy.prototype.checkCollisions = function()
     this.height/2.25 + this.y > player.y)
     {
       player.win=0;
+      //play audio
       audio_loser.play();
-      player.life--;
       setTimeout(function(){ player.reset(); }, 2000);
 
     }
@@ -62,71 +63,72 @@ Enemy.prototype.checkCollisions = function()
 
   Player.prototype.update = function () {
     if(this.y<=0)
-    {  this.win =1;
+    {  this.win =1;//set winner
       audio_winning.play();
       setTimeout(function(){
-          player.score++;
-           player.reset(); }, 2000);
+        player.score++;
+        player.reset(); }, 2000);
+      }
+
+    }
+    Player.prototype.render = function () {
+      ctx.drawImage(Resources.get(this.character), this.x, this.y);
+    }
+    Player.prototype.handleInput =function (key)
+    {
+      switch(key) {
+        case 'left':
+        if(this.x>0)
+        this.x = this.x - 100;
+        break;
+        case 'up':
+        if(this.y>39)
+        this.y = this.y - 90;
+        break;
+        case 'right':
+        if(this.x<400)
+        this.x = this.x + 100;
+        break;
+        case 'down':
+        if(this.y<400)
+        this.y = this.y + 90;
+        break;
+      }
+      audio_step.play();
     }
 
-  }
-  Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.character), this.x, this.y);
-  }
-  Player.prototype.handleInput =function (key)
-  {
-    switch(key) {
-      case 'left':
-      if(this.x>0)
-      this.x = this.x - 100;
-      break;
-      case 'up':
-      if(this.y>39)
-      this.y = this.y - 90;
-      break;
-      case 'right':
-      if(this.x<400)
-      this.x = this.x + 100;
-      break;
-      case 'down':
-      if(this.y<400)
-      this.y = this.y + 90;
-      break;
+    Player.prototype.reset =function() {
+      this.win=-1;//set to new game
+      this.x=200;
+      this.y=435;
     }
-    audio_step.play();
-  }
+    // Now instantiate your objects.
+    // Place all enemy objects in an array called allEnemies
+    // Place the player object in a variable called player
+    //get chosen character and create player object
+    if (window.location.href.split('=')[1]=='girl')
+    var player = new Player('images/char-pink-girl.png');
+    else
+    var player = new Player('images/char-boy.png');
+    var enemy1 = new Enemy(400,220);
+    var enemy2 = new Enemy(0,220);
+    var enemy3 = new Enemy(220,140);
+    var enemy4 = new Enemy(100,50);
+    var enemy5 = new Enemy(300,150);
+    var allEnemies = [enemy1,enemy2,enemy3,enemy4,enemy5];
+    //declare game audios
+    var audio_loser = new Audio('sounds/loser.mp3');
+    var audio_winning = new Audio('sounds/winning.mp3');
+    var audio_step = new Audio('sounds/step.wav');
+    // This listens for key presses and sends the keys to your
+    // Player.handleInput() method. You don't need to modify this.
+    document.addEventListener('keyup', function(e) {
+      var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+      };
 
-
-  Player.prototype.reset =function() {
-    this.win=-1;
-    this.x=200;
-    this.y=435;
-  }
-  // Now instantiate your objects.
-  // Place all enemy objects in an array called allEnemies
-  // Place the player object in a variable called player
-  if (window.location.href.split('=')[1]=='girl')
-  var player = new Player('images/char-pink-girl.png');
-  else
-  var player = new Player('images/char-boy.png');
-  var enemy1 = new Enemy(400,220);
-  var enemy2 = new Enemy(0,220);
-  var enemy3 = new Enemy(220,140);
-  var enemy4 = new Enemy(100,50);
-  var enemy5 = new Enemy(300,150);
-  var allEnemies = [enemy1,enemy2,enemy3,enemy4,enemy5];
-  var audio_loser = new Audio('sounds/loser.mp3');
-  var audio_winning = new Audio('sounds/winning.mp3');
-  var audio_step = new Audio('sounds/step.wav');
-  // This listens for key presses and sends the keys to your
-  // Player.handleInput() method. You don't need to modify this.
-  document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-      37: 'left',
-      38: 'up',
-      39: 'right',
-      40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
-  });
+      player.handleInput(allowedKeys[e.keyCode]);
+    });
--- js/launch.js
+++ js/launch.js
@@ -1,36 +1,36 @@
 
 var init = function () {
-var canvas= document.getElementById('launch-screen');
-var ctx = canvas.getContext('2d')
-ctx.fillStyle = "#428711";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.drawImage(Resources.get('images/bg.png'), 0, 0);
+  var canvas= document.getElementById('launch-screen');
+  var ctx = canvas.getContext('2d')
+  ctx.fillStyle = "#428711";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(Resources.get('images/bg.png'), 0, 0);
   ctx.drawImage(Resources.get('images/char-boy.png'), 160, 250);
   ctx.drawImage(Resources.get('images/char-pink-girl.png'), 260, 250);
   ctx.font = "bold 20px Arial";
   ctx.fillText("Press 1 to chose Boy and press 2 to chose Girl",20,430);
 }
-  Resources.load([
-      'images/char-boy.png',
-      'images/char-pink-girl.png',
-      'images/bg.png'
-  ]);
-  Resources.onReady(init);
+Resources.load([
+  'images/char-boy.png',
+  'images/char-pink-girl.png',
+  'images/bg.png'
+]);
+Resources.onReady(init);
 
-  document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function(e) {
 
-    var allowedKeys = {
-      49: 'boy',
-      50: 'girl'
-    };
-    var character;
-    switch (allowedKeys[e.keyCode]) {
-      case 'girl':
-      character = 'girl';
-      break;
-      case 'boy':
-      character = 'boy';
-      break;
-    };
-    window.location.href ="arcade.html?char="+character;
-  });
+  var allowedKeys = {
+    49: 'boy',
+    50: 'girl'
+  };
+  var character;
+  switch (allowedKeys[e.keyCode]) {
+    case 'girl':
+    character = 'girl';
+    break;
+    case 'boy':
+    character = 'boy';
+    break;
+  };
+  window.location.href ="arcade.html?char="+character;
+});
