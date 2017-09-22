78415c1ae36d5aebe37dd5a719562eb450ec7da8
Haifa <hma@Haifas-MacBook-Pro.local>
2017-09-22 16:13:27 +0300

    clear canvase

--- js/engine.js
+++ js/engine.js
@@ -125,6 +125,8 @@ var Engine = (function(global) {
   * they are just drawing the entire screen over and over.
   */
   function render() {
+    //Clear Canvas
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
     /* This array holds the relative URL to the image used
     * for that particular row of the game level.
     */
